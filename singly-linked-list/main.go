41894a61bfeb7edb23116820126a72c302f0a94f
ProgrammerBuffalo <eldareyvazov@gmail.com>
2023-11-09 21:45:01 +0400

    other: remove import fmt in singly linked list

--- singly-linked-list/main.go
+++ singly-linked-list/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	list := SinglyLinkedList[int]{}
 
@@ -12,16 +8,14 @@ func main() {
 	list.Add(3)
 	list.Add(4)
 
-	fmt.Println("Initial list")
 	list.Print()
 
 	list.Remove(3)
 
-	fmt.Println("After removed element")
 	list.Print()
 
 	list.Add(5)
-	fmt.Println("After added element")
+
 	list.Print()
 
 }
